2b02765062c0d1f2a85320c7590ec606536c69d9
smtomes <smtomes@gmail.com>
2023-06-23 10:46:22 -0500

    Update ninja_log_capture.ps1

--- ninja_log_capture.ps1
+++ ninja_log_capture.ps1
@@ -1,18 +1,18 @@
 $filePath = Get-ChildItem -Path "C:\Program Files (x86)" -Recurse -Filter "NinjaRMMAgent.exe" | Select-Object -ExpandProperty FullName
 
 if ($filePath.Count -eq 1) {
-    $directory = Split-Path -Path $filePath -Parent
-    Set-Location -Path $directory
-    
-    # Run ninjarmmagent.exe /collectlogs
-    Start-Process -FilePath "ninjarmmagent.exe" -ArgumentList "/collectlogs" -Wait
-
     # Remove all files and folders recursively from C:\Temp
     $logPath = "C:\Temp"
     if (Test-Path -Path $logPath) {
         Remove-Item -Path $logPath -Recurse -Force
     }
     
+    $directory = Split-Path -Path $filePath -Parent
+    Set-Location -Path $directory
+    
+    # Run ninjarmmagent.exe /collectlogs
+    Start-Process -FilePath "ninjarmmagent.exe" -ArgumentList "/collectlogs" -Wait
+
     # Ensure that C:\Temp folder exists
     if (-not (Test-Path -Path $logPath)) {
         New-Item -ItemType Directory -Path $logPath | Out-Null
