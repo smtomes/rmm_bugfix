1c3a6d1a8f2f542c4332f7088a988e1e2c36f06a
smtomes <smtomes@gmail.com>
2023-06-23 11:30:06 -0500

    Update ninja_log_capture.ps1

--- ninja_log_capture.ps1
+++ ninja_log_capture.ps1
@@ -10,8 +10,8 @@ if ($filePath.Count -eq 1) {
     $directory = Split-Path -Path $filePath -Parent
     Set-Location -Path $directory
     
-    # Run ninjarmmagent.exe /collectlogs silently
-    Start-Process -FilePath "ninjarmmagent.exe" -ArgumentList "/collectlogs" -ErrorAction SilentlyContinue -Wait
+    # Run ninjarmmagent.exe /collectlogs
+    Start-Process -FilePath "ninjarmmagent.exe" -ArgumentList "/collectlogs" -Wait
 
     # Ensure that C:\Temp folder exists
     if (-not (Test-Path -Path $logPath)) {
@@ -34,3 +34,26 @@ if ($filePath.Count -eq 1) {
 else {
     Write-Host "Multiple occurrences of NinjaRMMAgent.exe found."
 }
+
+# Simulate Enter key press to close the "Server execution failed" popup
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class KeystrokeSender {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        public static void SendEnterKey() {
+            const byte VK_RETURN = 0x0D;
+            const uint KEYEVENTF_KEYDOWN = 0x0000;
+            const uint KEYEVENTF_KEYUP = 0x0002;
+
+            keybd_event(VK_RETURN, 0x1C, KEYEVENTF_KEYDOWN, UIntPtr.Zero);
+            keybd_event(VK_RETURN, 0x1C, KEYEVENTF_KEYUP, UIntPtr.Zero);
+        }
+    }
+"@
+
+# Send Enter key press to close the popup window
+[KeystrokeSender]::SendEnterKey()
